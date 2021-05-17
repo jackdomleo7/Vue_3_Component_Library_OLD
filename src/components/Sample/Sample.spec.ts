546a3f66013e9ba690c9479c0807840a21ee9af8
Jack Domleo <jackdomleo7@gmail.com>
2021-05-17 19:19:44 +0100

    Added jest-axe

--- src/components/Sample/Sample.spec.ts
+++ src/components/Sample/Sample.spec.ts
@@ -1,5 +1,14 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import { JSample } from '@/components'
+import { configureAxe, toHaveNoViolations } from 'jest-axe'
+
+expect.extend(toHaveNoViolations)
+
+const axe = configureAxe({
+  rules: {
+    region: { enabled: false } // Don't need to check because components are loaded in isolation
+  }
+})
 
 describe('Sample.vue', () => {
   /**
@@ -26,5 +35,15 @@ describe('Sample.vue', () => {
     const wrapper = shallowMount(JSample)
 
     expect(wrapper.element).toMatchSnapshot()
+  })
+
+  /**
+   * A11y
+   */
+
+  it('to have no obvious accessibility violations', async () => {
+    const wrapper = mount(JSample)
+
+    expect(await axe(wrapper.element)).toHaveNoViolations()
   })
 })
