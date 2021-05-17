a9ce61536d3b6bb9c027610f48cc9e3d158a66f9
Jack Domleo <jackdomleo7@gmail.com>
2021-05-17 08:26:20 +0100

    Fix type

--- component-library.d.ts
+++ component-library.d.ts
@@ -4,4 +4,4 @@ import { DefineComponent, Plugin } from 'vue';
 declare const ComponentLibrary: Exclude<Plugin['install'], undefined>;
 export default ComponentLibrary;
 
-export const ComponentLibrarySample: DefineComponent<{}, {}, any>;
+export const JSample: DefineComponent<{}, {}, any>;
